a457090481168fb6e43305e677a36d1e77baf65c
kathelan <mateuszdeptulski@gmail.com>
2023-09-07 10:06:19 +0200

    backend init

---
+++ backend/src/test/java/pl/kathelan/backend/BackendApplicationTests.java
@@ -0,0 +1,20 @@
+package pl.kathelan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Test
+    void contextLoads() {
+        assertThat(ctx).isNotNull();
+    }
+}
